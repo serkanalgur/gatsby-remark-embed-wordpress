f3e9f9c26ab80f3fa99dfa6356cbca7f88878ba9
Serkan Algur <info@wpadami.com>
2018-07-18 00:50:06 +0300

    v 1.0.3

--- src/index.js
+++ src/index.js
@@ -11,19 +11,6 @@ module.export = ({markdownAST}, options = { width: 600, height: 380 }) => {
 		else return true
 	}
 
-	function urlFind(vals) {
-		urlVars = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}/g;
-
-		return vals.replace(urlVars, function(url){
-			return url;
-		})
-	}
-
-	function createoEmbedUri(validUri){
-		//Make WordPress oEmbedURL with func
-		return urlFind(validUri)+'/wp-json/oembed/1.0/embed?url='+urlEncode(validUri)+'&format=json';
-	}
-
 	visit(markdownAST, `inlineCode`, node => {
 		const {value} = node
 
@@ -31,17 +18,8 @@ module.export = ({markdownAST}, options = { width: 600, height: 380 }) => {
 			const WpostUrl = value.substr(8)
 
 			if(validUrlCheck(WpostUrl)){
-
-				var dok = request({
-					url : createoEmbedUri(WpostUrl),
-					json : true
-				}, function (error, response, body) {
-					if (!error && response.statusCode === 200) {
-						console.log(body) 
-					}
-				})
-				node.type = `html`
-				node.value = dok
+				node.type = `html`;
+				node.value = `<iframe sandbox="allow-scripts" security="restricted" src="${WpostUrl}/embed" width="${options.width}" height="${options.height}" title="" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="wp-embedded-content"></iframe><a href="${WpostUrl}" style="position:absolute; top:0; left:0; display:inline-block; width:${options.width}px; height:${options.height}px; z-index:5;"></a>`;
 			}
 		}
 	})
